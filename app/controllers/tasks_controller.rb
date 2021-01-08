29c51856d78f166574846186b009454334e761c7
keji-m <ken2mor@gmail.com>
2021-01-08 14:27:14 +0000

    fix tasks-controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,10 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index]
+  before_action :set_task, only: [:show, :edit, :update]
+  before_action :require_user_logged_in, only: [:index, :show, :new]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    if logged_in?
-      @task = current_user.tasks.build
-      @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(5)
-    end
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(5)
   end
   
   def show
@@ -43,9 +41,8 @@ class TasksController < ApplicationController
   
   def destroy
     @task.destroy
-    
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_path
   end
 
   private
@@ -57,6 +54,13 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
 end
